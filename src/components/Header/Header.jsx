958cd6ee3b68f3d5d71096d8125c91d9b759dab1
unknown <leandrosoares@gmail.com>
2026-01-31 13:23:09 -0300

    Estrutura

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,20 @@
 import { useState } from "react";
 import "./header.css";
+import { useEffect } from "react";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
 
   const handleClick = () => setOpen(false);
 
+  useEffect(() => {
+    document.body.style.overflow = open ? "hidden" : "auto";
+  }, [open]);
+
   return (
     <header className="header">
       <div className="header__container">
         <div className="logo">Leandro<span className="point">.</span></div>
-
-        <nav className={`nav ${open ? "open" : ""}`}>
-          <a href="#home" onClick={handleClick}></a>
-          <a href="#services" onClick={handleClick}>Serviços</a>
-          <a href="#portfolio" onClick={handleClick}>Portfólio</a>
-          <a href="#about" onClick={handleClick}>Sobre</a>
-        </nav>
 
         <button
           className={`menu-toggle ${open ? "open" : ""}`}
@@ -28,6 +26,14 @@ export default function Header() {
           <span />
         </button>
       </div>
+
+      <nav className={`nav ${open ? "open" : ""}`}>
+        <a href="#home" onClick={handleClick}></a>
+        <a href="#services" onClick={handleClick}>Serviços</a>
+        <a href="#portfolio" onClick={handleClick}>Portfólio</a>
+        <a href="#about" onClick={handleClick}>Sobre</a>
+      </nav>
+
     </header>
   );
 }
